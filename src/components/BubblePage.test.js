1e5fbb3625333e0699de5904a0a29c51a0cdaeab
Jesse-Leegwater <Jleegwater@live.com>
2021-08-20 17:40:58 -0700

    finished tests

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,102 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
-import { render, screen} from "@testing-library/react";
-import BubblePage from './BubblePage';
+import { render, screen } from "@testing-library/react";
+import BubblePage from "./BubblePage";
+import fetchColorService from "../services/fetchColorService";
 
-test("Renders without errors", ()=> {
-    
+jest.mock("../services/fetchColorService");
+
+const testList = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  },
+  {
+    color: "aquamarine",
+    code: {
+      hex: "#7fffd4",
+    },
+    id: 4,
+  },
+  {
+    color: "lilac",
+    code: {
+      hex: "#9a99dd",
+    },
+    id: 5,
+  },
+  {
+    color: "softpink",
+    code: {
+      hex: "#dd99ba",
+    },
+    id: 6,
+  },
+  {
+    color: "bisque",
+    code: {
+      hex: "#dd9a99",
+    },
+    id: 7,
+  },
+  {
+    color: "softyellow",
+    code: {
+      hex: "#dcdd99",
+    },
+    id: 8,
+  },
+  {
+    color: "blanchedalmond",
+    code: {
+      hex: "#ffebcd",
+    },
+    id: 9,
+  },
+  {
+    color: "blue",
+    code: {
+      hex: "#6093ca",
+    },
+    id: 10,
+  },
+  {
+    color: "blueviolet",
+    code: {
+      hex: "#8a2be2",
+    },
+    id: 11,
+  },
+];
+
+test("Renders without errors", () => {
+  fetchColorService.mockResolvedValueOnce({ testList });
+  render(<BubblePage />);
 });
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this component.
+test("Renders appropriate number of colors passed in through mock", async () => {
+  //Keep in mind that our service is called on mount for this component.
+  fetchColorService.mockResolvedValueOnce({ data: testList });
+
+  render(<BubblePage />);
+
+  expect(await screen.findAllByTestId("color")).toHaveLength(testList.length);
 });
